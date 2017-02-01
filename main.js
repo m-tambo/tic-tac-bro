912ba0fd928cfaebba1b7cf610d7ba9ee035c50c
j-west <westja89@gmail.com>
2017-02-01 09:42:43 -0600

    Change the checkBro function.  Also added logic so after a bro-spot is clicked, it cannot be changed during the same game

--- main.js
+++ main.js
@@ -10,96 +10,43 @@ const broBoard = document.querySelector('.bro-board');
 const broSpots = document.querySelectorAll('.bro-spot');
 const broAgain = document.querySelector('.center-bro');
 let currentBro = 'images/broBilly.jpeg';
-
+const checkBro = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8 ], [0, 4, 8], [2, 4, 6]];
 const broSpotRef = firebase.database().ref().child('Bro-Spots');
 
 
+function checkBros() {
+  const bros = document.querySelectorAll('img');
+  for (let i = 0; i < checkBro.length; i++) {
+    if (bros[checkBro[i][0]].attributes[0].value === currentBro && bros[checkBro[i][1]].attributes[0].value === currentBro && bros[checkBro[i][2]].attributes[0].value === currentBro) {
+      console.log('winner', currentBro);
+      broAgain.classList.remove('hidden-bro');
+      broAgain.addEventListener('click', initializeBros);
+    }
+  }
+}
 
 function initializeBros() {
+  broAgain.classList.add('hidden-bro');
   broSpots.forEach(function (broSpot) {
     const broClass = broSpot.classList[1];
     broSpotRef.update({[broClass] : 'images/bro.png'})
   })
 }
 
-function checkBros() {
-  const bros = document.querySelectorAll('img');
-  console.log(bros)
-  if(bros[0].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[1].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[2].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[3].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[5].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[6].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[7].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[8].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[0].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[3].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[6].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[1].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[7].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[2].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[5].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[8].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[0].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[8].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[2].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[6].attributes[0].value === 'images/broBilly.jpeg') {
-
-    console.log('broBilly has taken the day!');
-    broAgain.classList.remove('hidden-bro');
-    broAgain.addEventListener('click', initializeBros);
-  }
-  else if(bros[0].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[1].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[2].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[3].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[5].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[6].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[7].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[8].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[0].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[3].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[6].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[1].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[7].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[2].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[5].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[8].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[0].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[8].attributes[0].value === 'images/broBilly.jpeg' ||
-    bros[2].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[4].attributes[0].value === 'images/broBilly.jpeg' &&
-    bros[6].attributes[0].value === 'images/broBilly.jpeg') {
-
-    console.log('broLeggs has taken the day!');
-    broAgain.classList.remove('hidden-bro');
-    broAgain.addEventListener('click', initializeBros);
-  }
-}
-
 function broClick(e) {
-
-  if(currentBro === 'images/broBilly.jpeg') {
-    currentBro = 'images/broLeggs.jpeg'
-  } else {
-    currentBro = 'images/broBilly.jpeg'
-  }
-  if(e.target.tagName === 'IMG') {
-    let broId = e.target.id;
-    //console.log(broId);
-    broSpotRef.update({[broId] : currentBro});
-    //initializeBros();
+  console.log(e)
+  if (e.target.attributes[0].value === 'images/bro.png') {
+    if (currentBro === 'images/broBilly.jpeg') {
+      currentBro = 'images/broLeggs.jpeg'
+    } else {
+      currentBro = 'images/broBilly.jpeg'
+    }
+    if (e.target.tagName === 'IMG') {
+      let broId = e.target.id;
+      broSpotRef.update({ [broId]: currentBro });
+    }
   }
 }
-
 
 
 broBoard.addEventListener('click', broClick);
